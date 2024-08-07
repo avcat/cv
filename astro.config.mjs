79f74f05c53cf9dffd41d709dc6df520c38ba3a6
arkadii.vodolazskyi <49953024+avcat@users.noreply.github.com>
2024-08-07 22:18:18 +0300

    Test no base URL.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://avcat.github.io',
-  base: 'arkadii.vodolazskyi'
+  site: 'https://avcat.github.io'
 });
