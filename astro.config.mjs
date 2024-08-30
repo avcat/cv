1ed16d4fcfbfce7962bdbe6d6e386ca6e41735e2
arkadii.vodolazskyi <49953024+avcat@users.noreply.github.com>
2024-08-30 23:36:31 +0300

    Updated base URL. Added Link component.
    Reason: Astro does not provide a way to conditionally use base from the config: https://github.com/withastro/roadmap/discussions/292.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://avcat.github.io'
+  site: 'https://avcat.github.io',
+  base: '/arkadii.vodolazskyi',
 });
