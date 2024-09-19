44b5b2d346227c9129226e6a62fbe228499bf821
arkadii.vodolazskyi <49953024+avcat@users.noreply.github.com>
2024-09-19 22:59:18 +0300

    Splitted into components, data, files, templates, etc.
    Todo: create other components -> move data into data.js -> use components in PageA4 -> create a map with PageA4 in index.

--- src/helpers/data.js
+++ src/helpers/data.js
@@ -1,27 +1,43 @@
-// @ts-check
-
-/**
- * @typedef {Object} Section
- * @property {string} [title]
- * @property {Object[]} data
- * 
- * @typedef {Section[]} Page
- * 
- * @typedef {Page[]} Pages
- * 
- * @type {Pages}
- */
 export const pages = [
   // Page 1
-  [
-    
-  ],
+  [ // Sections
+    {
+      type: 'introduction',
+      data: {
+        image: '@images/cv/av.webp',
+        name: 'Arkadii Vodolazskyi',
+        position: 'WordPress Developer',
+      }
+    },
+    {
+      type: 'contacts',
+      title: 'Contacts',
+      data: [
+        {
+          url: 'https://dub.sh/zMqXUw2',
+          icon: 'proton', 
+          title: 'Email', 
+        },
+        {
+          url: 'https://www.linkedin.com/in/avcat',
+          icon: 'linkedin', 
+          title: 'LinkedIn', 
+        },
+        {
+          url: 'https://github.com/avcat',
+          icon: 'github', 
+          title: 'GitHub', 
+        },
+        {
+          url: 'https://dub.sh/i9CebbJ',
+          icon: 'telegram', 
+          title: 'Telegram', 
+        },
+      ]
+    }
+  ]
+  
   // Page 2
-  [
-    
-  ],
+
   // Page 3
-  [
-    
-  ],
 ];
