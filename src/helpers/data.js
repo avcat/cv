53d6aeabc96361d9f6e1fe410e44804727a2eab2
arkadii.vodolazskyi <49953024+avcat@users.noreply.github.com>
2024-09-19 22:16:18 +0300

    Added explicit sections Nav, Introduction.

---
+++ src/helpers/data.js
@@ -0,0 +1,27 @@
+// @ts-check
+
+/**
+ * @typedef {Object} Section
+ * @property {string} [title]
+ * @property {Object[]} data
+ * 
+ * @typedef {Section[]} Page
+ * 
+ * @typedef {Page[]} Pages
+ * 
+ * @type {Pages}
+ */
+export const pages = [
+  // Page 1
+  [
+    
+  ],
+  // Page 2
+  [
+    
+  ],
+  // Page 3
+  [
+    
+  ],
+];
--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,1 +1,8 @@
 export const sanitizeUrl = (url) => url.replace(/^(https?:\/\/)?(www\.)?/, '').replace(/\/$/, '');
+
+/**
+ * Slugify a word or phrase by making it lowercase and replacing spaces with dashes.
+ * @param {string} text
+ * @returns {string}
+ */
+export const slugifyTitle = (text) => text.toLowerCase().trim().replace(/\s+/g, '-');
